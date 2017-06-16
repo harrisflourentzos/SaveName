f68b8077207659fd15635a7bb6e02cc585dd2a7c
harrisflourentzos <lrentzo227@gmail.com>
2017-06-16 10:56:21 +0200

    populate MainActivity final version - works fine

--- app/src/main/java/savename/myandroidhello/com/savename/MainActivity.java
+++ app/src/main/java/savename/myandroidhello/com/savename/MainActivity.java
@@ -1,17 +1,20 @@
 package savename.myandroidhello.com.savename;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button saveButton;
     private TextView showName;
     private EditText enterName;
+    private static final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                SharedPreferences myPrefs= getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = myPrefs.edit();
+
+                if(enterName.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Please Insert a Name !!",Toast.LENGTH_LONG).show();
+                }else{
+                    editor.putString("name", enterName.getText().toString());
+                    editor.commit();
+                }
+
             }
         });
+
+        //get date back:
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+
+        if(prefs.contains("name")){
+            String userName = prefs.getString("name", "not found");
+            showName.setText("You are " + userName);
+        }else{
+            showName.setText("");
+        }
 
     }
 }
